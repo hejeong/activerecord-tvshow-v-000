2b5ca6f23e63c3c0d861855978646a0d0f09c9f7
Jonathan Hong <jonhong777@gmail.com>
2018-08-08 15:12:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,15 +1,15 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
     rating = self.highest_rating
-    Show.find_by("rating = ?", rating)
+    self.find_by("rating = ?", rating)
   end
 
   def self.lowest_rating
-
+    
   end
 
   def self.least_popular_show
